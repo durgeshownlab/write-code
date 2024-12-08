f4dd051d6c09ab15bad58afd37fb534a7e193df6
Durgesh Singh <durgeshownlab@gmail.com>
2024-12-09 02:41:39 +0530

    update vite.config for allowing unused var

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/iframe-content': {
-        target: 'http://localhost:5173/',
-        changeOrigin: true,
-        secure: false,
-      }
-    },
-  }
+  build: {
+    minify: true,
+    terserOptions: {
+      compress: {
+        keep_fargs: true,
+        keep_fnames: true
+      },
+    }
+  },
 })
